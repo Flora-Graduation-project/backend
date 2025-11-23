9f18706dc580af49e7111c63f11d0e24a6f2a860
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2025-11-23 21:56:41 +0200

    Add user model schema for user data management

---
+++ DB/Models/User/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+    },
+
+    password: String,
+
+    profilePic: {secure_url:String,public_id:String},
+   
+
+    // isComfirmed: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+
+    // isLoggedIn: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
