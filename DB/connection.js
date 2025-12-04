43cd3408ecbdd4b40e889a5e5150def5f7c7453a
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2025-12-04 15:18:23 +0200

    refactor database connection logic to use caching for improved performance

--- DB/connection.js
+++ DB/connection.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 export const connectDB = async () => {
-  return await mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-      console.log("connected to db .....");
-    })
-    .catch((err) => {
-      console.log("Error in connecting to db", err.message);
-    });
+  if (cached.conn) {
+    // إذا الاتصال موجود مسبقًا، استخدمه مباشرة
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    // لو مفيش اتصال جاري، نعمل اتصال جديد
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("Connected to DB ...");
+  return cached.conn;
 };
